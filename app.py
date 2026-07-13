2a4bb53bec60b0e88955dc788f48f15319079851
kj <keydiyey@gmail.com>
2026-07-13 13:24:56 +0800

    updated threshold

--- app.py
+++ app.py
@@ -2,8 +2,31 @@ import streamlit as st
 import pandas as pd
 import io
 import plotly.graph_objects as go
+import numpy as np
 
 st.set_page_config(page_title="sefram?", layout="wide")
+
+
+st.markdown(
+    """
+    <style>
+    .top-banner {
+        background-color: #1E3A8A;
+        color: white;
+        padding: 15px;
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+        border-radius: 5px;
+        margin-bottom: 20px;
+    }
+    </style>
+    <div class="top-banner">
+        RELIABILITY ENGINEERING
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 with st.sidebar:
     uploaded_file = st.file_uploader("Choose a file", type=["csv", "txt"])
@@ -56,9 +79,69 @@ if uploaded_file is not None:
     if df_final is None:
         st.error(channel_cols)
     else:
-        st.subheader("graph")
+        df_final['Mean_Temp'] = df_final[channel_cols].mean(axis=1)
+        
+        df_final['Smoothed_Temp'] = df_final['Mean_Temp'].rolling(window=5, center=True, min_periods=1).mean()
+        
+
+        df_final['Slope'] = df_final['Smoothed_Temp'].diff().fillna(0)
+        
+
+        slope_threshold = 0.25
+        
+        conditions = [
+            (df_final['Slope'] > slope_threshold),
+            (df_final['Slope'] < -slope_threshold)
+        ]
+        choices = ['Ramp Up', 'Ramp Down']
+        df_final['Phase'] = np.select(conditions, choices, default='Dwell') 
+        
+
+        df_final['Phase_Change'] = (df_final['Phase'] != df_final['Phase'].shift(1)).cumsum()
+        
+        phase_blocks = []
+        for _, group in df_final.groupby('Phase_Change'):
+            phase_type = group['Phase'].iloc[0]
+            start_time = group['Duration (min)'].min()
+            end_time = group['Duration (min)'].max()
+            duration = end_time - start_time + 1
+            avg_temp = group['Mean_Temp'].mean()
+            
+            if phase_type == 'Dwell':
+                phase_type = 'Hot Dwell' if avg_temp > 25 else 'Cold Dwell'
+                
+            phase_blocks.append({
+                "Phase": phase_type,
+                "Start (min)": start_time,
+                "End (min)": end_time,
+                "Duration (min)": duration,
+                "Avg Temp (°C)": round(avg_temp, 1)
+            })
+            
+        df_phases = pd.DataFrame(phase_blocks)
+        
+        new_df = df_phases[df_phases["Duration (min)"] > 10]
+            
+
+        st.subheader("Graph")
         
         fig = go.Figure()
+        
+      
+        color_map = {
+            'Ramp Up': 'rgba(211, 211, 211, 0)',
+            'Ramp Down': 'rgba(211, 211, 211, 0)', 
+            'Hot Dwell': 'rgba(211, 211, 211, 0.5)',
+            'Cold Dwell': 'rgba(211, 211, 211, 0.5)' 
+        }
+        
+        for block in phase_blocks:
+            fig.add_vrect(
+                x0=block["Start (min)"], x1=block["End (min)"],
+                fillcolor=color_map.get(block["Phase"], "rgba(0,0,0,0)"),
+                layer="below", line_width=0
+            )
+        
         for col in channel_cols:
             fig.add_trace(go.Scatter(
                 x=df_final['Duration (min)'], 
@@ -67,18 +150,20 @@ if uploaded_file is not None:
                 name=col
             ))
             
-        
-        thresholds = [78, 80, 82, -28, -30, -32]
-
-        # add aut
+        if "TC" in uploaded_file.name or "HF" in uploaded_file.name:
+            thresholds = [120, -40]
+        elif "DH" in uploaded_file.name:
+            thresholds = [85]
+        else:
+            thresholds = [120]
+            
+        def addThresholdLine(y):
+            return fig.add_hline(y=y, line_dash="dash", line_color="red", line_width=1)
 
         for t in thresholds:
-            fig.add_hline(
-                y=t, 
-                line_dash="dash", 
-                line_color="red", 
-                line_width=1
-            )
+            addThresholdLine(t + 2)
+            addThresholdLine(t)
+            addThresholdLine(t - 2)
 
         fig.add_hline(y=25, line_color="blue", line_width=1.5)
             
@@ -86,34 +171,24 @@ if uploaded_file is not None:
             xaxis_title="Duration (min)",
             yaxis_title="Temperature (°C)",
             hovermode="x unified",
-            margin=dict(l=40, r=40, t=20, b=40),
+            margin=dict(l=40, r=40, t=40, b=40),
             legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
             xaxis=dict(
-                showgrid=True,
-                gridwidth=1,
-                gridcolor='rgba(180, 180, 180, 0.6)',
-                nticks=30,
-                minor=dict(
-                    showgrid=True,
-                    gridwidth=0.5,
-                    gridcolor='rgba(210, 210, 210, 0.4)'
-                )
+                showgrid=True, gridwidth=1, gridcolor='rgba(180, 180, 180, 0.6)', nticks=30,
+                minor=dict(showgrid=True, gridwidth=0.5, gridcolor='rgba(210, 210, 210, 0.4)')
             ),
             yaxis=dict(
-                showgrid=True,
-                gridwidth=1,
-                gridcolor='rgba(180, 180, 180, 0.6)',
-                nticks=20,
-                minor=dict(
-                    showgrid=True,
-                    gridwidth=0.5,
-                    gridcolor='rgba(210, 210, 210, 0.4)'
-                )
+                showgrid=True, gridwidth=1, gridcolor='rgba(180, 180, 180, 0.6)', nticks=20,
+                minor=dict(showgrid=True, gridwidth=0.5, gridcolor='rgba(210, 210, 210, 0.4)')
             ),
             template="plotly_white"
         )
         
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, width='stretch')
+        
+
+        st.subheader("summary")
+        st.dataframe(new_df, width='stretch', hide_index=True)
             
-        with st.expander("cleaned data", expanded=False):
-            st.dataframe(df_final, use_container_width=True)
+        with st.expander("Cleaned Raw Data", expanded=False):
+            st.dataframe(df_final[['Duration (min)'] + channel_cols], width='stretch')
